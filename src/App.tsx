b3e92b67020f1bc29bda0ae7a2492f98b7039c38
Andrés <lopez.andresrigoberto@gmail.com>
2023-04-30 19:23:24 -0500

    [edit] route's components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './actions/views/Home';
-import Auth from './actions/views/Auth';
+import Login from './actions/views/Login';
+import Signup from './actions/views/Signup';
+import SetImg from './actions/views/SetImg';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
-      <Route path='/auth' element={<Auth />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/signup' element={<Signup />}></Route>
+      <Route path='/setimg' element={<SetImg />}></Route>
     </Routes>
   );
 }
