30fd260e7bad3a1198e8214f9830cba4deb96a31
Andrés <lopez.andresrigoberto@gmail.com>
2023-05-07 13:23:30 -0500

    [wip] name  the animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,7 +56,7 @@ export default {
           '0%': { transform: 'translateX(40%)', opacity: 0 },
           '100%': { transform: 'translateX(0)', opacity: 1 },
         },
-        show: {
+        scale: {
           '0%': { transform: 'scale(0)', opacity: 0 },
           '100%': { transform: 'scale(1)', opacity: 1 },
         },
@@ -64,11 +64,35 @@ export default {
           '0%': { transform: 'translateX(20%)', opacity: 0 },
           '100%': { transform: 'translateX(0)', opacity: 1 },
         },
+        scaleXY: {
+          '0%': {
+            transform: 'scale(0)',
+          },
+          '100%': {
+            transform: 'scale(1)',
+          },
+        },
+        scaleWidth: {
+          '0%': { width: '0' },
+          '100%': { width: 'w-3/5' },
+        },
+        deScaleWidth: {
+          '0%': { width: 'w-3/5' },
+          '100%': { width: '0' },
+        },
+        swipeFromTop: {
+          '0%, 50%': { transform: 'translateY(-100%)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
       },
       animation: {
         mtl: 'moveToLeft 0.75s cubic-bezier(.13,.49,.42,1.35)',
-        show: 'show 0.75s cubic-bezier(.13,.49,.42,1.35)',
+        show: 'scale 0.75s cubic-bezier(.13,.49,.42,1.35)',
         pfl: 'openFromLeft 0.75s ease',
+        scale: 'scaleXY 0.2s',
+        scaleWidth: 'scaleWidth 0.5s ease',
+        deScaleWidth: 'deScaleWidth 0.5s ease',
+        swipeFromTop: 'swipeFromTop 0.5s ease-out',
       },
       boxShadow: {
         input: '0px 0px 6px 3px rgba(78,33,183,0.52)',
