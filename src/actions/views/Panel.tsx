cd7c2d88def1d311bf9bf137050bbec6dafc6070
Andrés <lopez.andresrigoberto@gmail.com>
2023-05-12 13:36:52 -0500

    [edit] use popUp context

--- src/actions/views/Panel.tsx
+++ src/actions/views/Panel.tsx
@@ -1,8 +1,17 @@
-import React, { useState, useRef, RefObject } from 'react';
+import React, {
+  useState,
+  useRef,
+  RefObject,
+  useContext,
+  useEffect,
+} from 'react';
 import LeftPanel from '../components/LeftPanel';
 import RightPanel from '../components/RightPanel';
+import { popUpsContext } from '../context/popUpContext';
 
 const Panel: React.FC = () => {
+  const { listOfPopUps, setPopUps, closePopUps } = useContext(popUpsContext);
+
   const chatPopUpRef = useRef<HTMLDivElement>(null);
   const chatIconRef = useRef<SVGSVGElement>(null);
   const setPopUpRef = useRef<HTMLDivElement>(null);
@@ -16,47 +25,25 @@ const Panel: React.FC = () => {
 
   const [chatId, setchatId] = useState<number | null>(null);
 
-  const [listOfPopUps, setListOfPopUps] = useState<
-    {
-      id: number;
-      state: boolean;
-      popup: RefObject<HTMLDivElement>;
-      icon: RefObject<SVGSVGElement>;
-    }[]
-  >([
-    { id: 0, state: false, popup: chatPopUpRef, icon: chatIconRef },
-    { id: 1, state: false, popup: setPopUpRef, icon: setIconRef },
-    { id: 2, state: false, popup: filePopUpRef, icon: fileIconRef },
-    { id: 3, state: false, popup: contactSetPopUpRef, icon: contactSetIconRef },
-  ]);
+  useEffect(() => {
+    setPopUps([
+      { id: 0, state: false, popup: chatPopUpRef, icon: chatIconRef },
+      { id: 1, state: false, popup: setPopUpRef, icon: setIconRef },
+      { id: 2, state: false, popup: filePopUpRef, icon: fileIconRef },
+      {
+        id: 3,
+        state: false,
+        popup: contactSetPopUpRef,
+        icon: contactSetIconRef,
+      },
+    ]);
+  }, []);
 
   const onSelectChatId = (id: number) => {
     closePopUps();
     if (id) {
       setchatId(id);
     }
-  };
-
-  const popUpHandler = (id: number) => {
-    const copy = listOfPopUps.map((x) => x);
-    copy.map((e) => {
-      if (e.id === id) {
-        if (e.state === false) {
-          e.state = true;
-        } else {
-          e.state = false;
-        }
-      } else {
-        e.state = false;
-      }
-    });
-    setListOfPopUps(copy);
-  };
-
-  const closePopUps = () => {
-    const copy = listOfPopUps.map((x) => x);
-    copy.map((e) => (e.state = false));
-    setListOfPopUps(copy);
   };
 
   const closePopUpWhenClickOutside = (
@@ -80,22 +67,8 @@ const Panel: React.FC = () => {
       className='relative flex flex-col items-center justify-center w-full h-screen p-10 bg-gradient-radial from-darkPurple to-brightPurple overflow-hidden'
     >
       <div className='flex w-11/12 h-400 bg-white'>
-        <LeftPanel
-          UserImage=''
-          chatObj={listOfPopUps[0]}
-          settingObj={listOfPopUps[1]}
-          popUpHandler={popUpHandler}
-          onSelectChatId={onSelectChatId}
-          closePopUps={closePopUps}
-        />
-
-        <RightPanel
-          chatId={chatId}
-          contactSetObj={listOfPopUps[2]}
-          fileObj={listOfPopUps[3]}
-          popUpHandler={popUpHandler}
-          closePopUps={closePopUps}
-        />
+        <LeftPanel UserImage='' onSelectChatId={onSelectChatId} />
+        <RightPanel chatId={chatId} />
       </div>
     </div>
   );
