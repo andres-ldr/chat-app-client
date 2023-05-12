8e712b0177783aee281a1834c17a32bfa24818c7
Andrés <lopez.andresrigoberto@gmail.com>
2023-05-12 13:35:59 -0500

    [add] popUpContext & provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from './actions/views/Login';
 import Signup from './actions/views/Signup';
 import SetImg from './actions/views/SetImg';
 import Panel from './actions/views/Panel';
+import { PopUpsProvider } from './actions/context/popUpContext';
 
 function App() {
   return (
@@ -12,7 +13,14 @@ function App() {
       <Route path='/login' element={<Login />}></Route>
       <Route path='/signup' element={<Signup />}></Route>
       <Route path='/setimg' element={<SetImg />}></Route>
-      <Route path='/panel' element={<Panel />}></Route>
+      <Route
+        path='/panel'
+        element={
+          <PopUpsProvider>
+            <Panel />
+          </PopUpsProvider>
+        }
+      ></Route>
     </Routes>
   );
 }
