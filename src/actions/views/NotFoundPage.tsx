f5f681fd883be3bb6674d668101c1233c1cf95be
Andrés <lopez.andresrigoberto@gmail.com>
2023-08-21 11:21:47 -0700

    [add] not found page

---
+++ src/actions/views/NotFoundPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <section className='min-w-full h-screen bg-slate-100 flex flex-col justify-center items-center space-y-52'>
+      <h1 className='text-8xl text-lightPurple font-semibold'>
+        404 | Not found
+      </h1>
+      <div className='text-3xl flex flex-col justify-center items-center space-y-10 font-medium'>
+        <h2 className='text-4xl'>Maybe your looking for:</h2>
+        <Link
+          to='/'
+          className='transition-all hover:font-semibold hover:text-lightPurple'
+        >
+          Home
+        </Link>
+        <Link
+          to='/login'
+          className='transition-all hover:font-semibold hover:text-lightPurple'
+        >
+          Log In
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundPage;
