d176410223c6c0b5003c52caf756a4ad0170d6af
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-01 18:43:53 -0700

    [add] redux provider and persist gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { AuthProvider } from './actions/context/AuthContext.tsx';
+import { persistor, store } from './actions/redux/store.ts';
 import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.tsx';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </PersistGate>
+    </Provider>
   </BrowserRouter>
 );
