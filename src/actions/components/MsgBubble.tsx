1a3406332a61b12e01dfce436212a17406525b30
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-01 19:31:23 -0700

    [add] inject msg from redux

--- src/actions/components/MsgBubble.tsx
+++ src/actions/components/MsgBubble.tsx
@@ -2,30 +2,23 @@ import React, { Fragment } from 'react';
 
 interface MsgBubbleProps {
   isRight?: boolean;
+  content: string;
 }
 
-const MsgBubble: React.FC<MsgBubbleProps> = ({ isRight }) => {
+const MsgBubble: React.FC<MsgBubbleProps> = ({ content, isRight }) => {
   return (
     <Fragment>
       {isRight ? (
         <div className='w-full flex justify-end p-5'>
           <div className='h-full max-w-2xl inline-flex flex-col p-4 border-2 border-grayReg cursor-pointer bg-grayReg rounded-3xl text-xl'>
-            <p className='mb-2'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro non
-              sunt
-            </p>
+            <p className='mb-2'>{content}</p>
             <span className='self-end font-light'>12:03</span>
           </div>
         </div>
       ) : (
         <div className='w-full p-5'>
           <div className='h-full max-w-2xl inline-flex flex-col p-4 border-2 border-gray cursor-pointer bg-white rounded-3xl text-xl'>
-            <p className='mb-2'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro non
-              sunt sequi quo reiciendis, maiores, animi perspiciatis ratione
-              repellendus veritatis culpa optio ad mollitia incidunt aspernatur
-              ipsa velit, obcaecati eligendi!
-            </p>
+            <p className='mb-2'>{content}</p>
             <span className='self-end font-light'>12:03</span>
           </div>
         </div>
