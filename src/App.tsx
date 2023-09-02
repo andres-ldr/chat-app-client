ae7d2a070e6dae6fb2bc0e7b9462e75fc8a5a47c
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-01 18:43:12 -0700

    [add] signup-2 route and not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './actions/views/Home';
-import Login from './actions/views/Login';
-import Signup from './actions/views/Signup';
-import SetImg from './actions/views/SetImg';
-import Panel from './actions/views/Panel';
 import { PopUpsProvider } from './actions/context/popUpContext';
+import { selectUser } from './actions/redux/user/userSelector';
+import NotFoundPage from './actions/views/NotFoundPage';
+import { Routes, Route } from 'react-router-dom';
+import Signup2 from './actions/views/Signup2';
+import SetImg from './actions/views/SetImg';
+import Signup from './actions/views/Signup';
+import Login from './actions/views/Login';
+import Panel from './actions/views/Panel';
+import Home from './actions/views/Home';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/login' element={<Login />}></Route>
       <Route path='/signup' element={<Signup />}></Route>
       <Route path='/setimg' element={<SetImg />}></Route>
+      <Route path='/signup-step-2' element={<Signup2 />}></Route>( (
       <Route
         path='/panel'
         element={
@@ -21,6 +28,8 @@ function App() {
           </PopUpsProvider>
         }
       ></Route>
+      )
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
 }
