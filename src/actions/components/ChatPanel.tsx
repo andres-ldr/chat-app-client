ec90d7b3cb3e938726eecb69960cddd651a5193d
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-01 19:30:22 -0700

    [edit] popup components and msg redux state

--- src/actions/components/ChatPanel.tsx
+++ src/actions/components/ChatPanel.tsx
@@ -1,79 +1,90 @@
-import React, { useContext } from 'react';
-import PopUp from './PopUp';
-import MsgBubble from './MsgBubble';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { selectUser } from '../redux/user/userSelector';
+import { VALIDATOR_REQUIRE } from '../Util/validators';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectChat } from '../redux/chat/selectChat';
+import { postMsg } from '../redux/chat/chatSlice';
+import { AppDispatch } from '../redux/store';
+import { useForm } from '../hooks/form-hook';
 import {
   faEllipsisVertical,
   faPaperclip,
 } from '@fortawesome/free-solid-svg-icons';
-import { popUpsContext } from '../context/popUpContext';
+import Button from './FormElements/Button';
+import PopUpElement from './PopUpElement';
+import Input from './FormElements/input';
+import MsgBubble from './MsgBubble';
+import React from 'react';
 
-interface ChatPanelProps {
-  contactInfoPanelHandler: () => void;
-}
+const ChatPanel: React.FC = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      message: {
+        value: '',
+        isValid: false,
+      },
+    },
+    false
+  );
+  const { chat, cid, chatImage, alias } = useSelector(selectChat);
+  const { user } = useSelector(selectUser);
+  const dispatch = useDispatch<AppDispatch>();
 
-const ChatPanel: React.FC<ChatPanelProps> = ({ contactInfoPanelHandler }) => {
-  const { listOfPopUps, popUpHandler } = useContext(popUpsContext);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const message = formState.inputs.message.value;
+    dispatch(postMsg({ cid, content: message, type: 'TEXT' }));
+  };
 
   return (
-    <div className='w-full h-full flex flex-col bg-white'>
+    <div className={`flex w-full h-full  flex-col bg-white`}>
       {/* contact bar */}
       <div className='w-full h-32 flex justify-between bg-grayLight p-5'>
         <div className='flex'>
-          <img
-            src=''
-            alt=''
-            className='w-20 h-20 mr-6 circle cursor-pointer transition hover:backdrop-blur-lg hover:opacity-95'
-          />
-          <h3 className='text-3xl'>My friend</h3>
+          {chatImage ? (
+            <img
+              src={`http://localhost:8000/${chatImage}`}
+              alt=''
+              className='w-20 h-20 mr-6 circle cursor-pointer transition hover:backdrop-blur-lg hover:opacity-95'
+            />
+          ) : (
+            <div></div>
+          )}
+          <h3 className='text-3xl'>{alias}</h3>
         </div>
-        <div className='flex relative'>
+        <div className='flex'>
           {/* Info contact popup */}
-          <PopUp
-            isShown={listOfPopUps[3].state}
-            Ref={listOfPopUps[3].popup}
-            right={0}
-            content={[
-              {
-                id: 0,
-                label: 'More info',
-                action: () => contactInfoPanelHandler(),
-              },
-            ]}
-          />
-
-          {/* File popup */}
-          <PopUp
-            isShown={listOfPopUps[2].state}
-            Ref={listOfPopUps[2].popup}
-            right={16}
-            content={[
-              {
-                id: 0,
-                label: 'Add file',
-                action: () => console.log('ADD FILE'),
-              },
-            ]}
-          />
-
-          <FontAwesomeIcon
-            ref={listOfPopUps[2].icon}
-            icon={faPaperclip}
-            onClick={() => popUpHandler(2)}
-            className='w-10 h-10 cursor-pointer p-3 mr-3 transition rounded-full text-grayDark hover:bg-gray z-20'
-          />
-          <FontAwesomeIcon
-            ref={listOfPopUps[3].icon}
-            icon={faEllipsisVertical}
-            onClick={() => popUpHandler(3)}
-            className='w-10 h-10 cursor-pointer p-3 transition rounded-full text-grayDark hover:bg-gray z-20'
-          />
+          <PopUpElement icon={faPaperclip}>
+            <span
+              className='transition hover:bg-gray p-3'
+              onClick={() => {
+                /** */
+              }}
+            >
+              More info
+            </span>
+          </PopUpElement>
+          <PopUpElement icon={faEllipsisVertical}>
+            <span
+              className='transition hover:bg-gray p-3'
+              onClick={() => {
+                /** */
+              }}
+            >
+              Add file
+            </span>
+          </PopUpElement>
         </div>
       </div>
       {/* chat area */}
       <div className='w-full h-0 grow bg-white overflow-y-auto'>
-        <MsgBubble />
-        <MsgBubble isRight={true} />
+        {chat.map((e: any) => {
+          if (e.senderId === user.uid) {
+            return <MsgBubble key={e.mid} content={e.content} isRight={true} />;
+          } else {
+            return <MsgBubble key={e.mid} content={e.content} />;
+          }
+        })}
       </div>
       {/* Message input */}
       <div className='w-full h-24 flex justify-start items-center pl-5 pr-5 pt-3 pb-3 bg-grayDark'>
@@ -81,14 +92,25 @@ const ChatPanel: React.FC<ChatPanelProps> = ({ contactInfoPanelHandler }) => {
           icon={faPaperclip}
           className='w-10 h-10 cursor-pointer p-3 transition rounded-full text-grayLight mr-3 hover:bg-gray'
         />
-        <form action='' className='w-full h-full'>
-          <textarea
-            name=''
-            id=''
-            maxLength={255}
-            placeholder='Write a message'
-            className='w-full h-full rounded-xl outline-none placeholder-grayDark pl-6 pt-4 pr-4 pb-4 text-2xl  transition resize-none focus:shadow-input'
+        <form
+          className='flex w-full h-full space-x-5'
+          onSubmit={(event) => handleSubmit(event)}
+        >
+          <Input
+            element='textarea'
+            id='message'
+            type='text'
+            rows={1}
+            validators={[VALIDATOR_REQUIRE()]}
+            onInput={inputHandler}
           />
+          <Button
+            rounded={true}
+            onClick={handleSubmit}
+            disabled={!formState.isValid}
+          >
+            Send
+          </Button>
         </form>
       </div>
     </div>
