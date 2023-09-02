be69e6e6480a628539136924704cdd47d95e20d7
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-01 19:25:40 -0700

    [edit] inject redux states

--- src/actions/components/LeftPanel.tsx
+++ src/actions/components/LeftPanel.tsx
@@ -1,68 +1,75 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { clearChatOnLogOut } from '../redux/chat/chatSlice';
+import { selectUser } from '../redux/user/userSelector';
+import { useDispatch, useSelector } from 'react-redux';
+import { postLogOut } from '../redux/user/userSlice';
+import React, { useState } from 'react';
+import ChatsPanel from './chatsPanel/ChatsPanel';
+import { useNavigate } from 'react-router-dom';
+import { AppDispatch } from '../redux/store';
 import {
   faEllipsisVertical,
   faMagnifyingGlass,
   faMessage,
 } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useContext } from 'react';
-import PopUp from './PopUp';
-import ChatCard from './ChatCard';
-import { popUpsContext } from '../context/popUpContext';
+import ContactsPanel from './ContactsPanel';
+import PopUpElement from './PopUpElement';
 
-interface LeftPopUpProps {
-  UserImage: string;
-  onSelectChatId: (id: number) => void;
-}
+const LeftPanel: React.FC = () => {
+  const [isContactPanelOpen, setIsContactPanelOpen] = useState<boolean>(false);
+  const [isChatsPanelOpen, setIsChatsPanelOpen] = useState<boolean>(true);
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useSelector(selectUser);
 
-const LeftPanel: React.FC<LeftPopUpProps> = ({ UserImage, onSelectChatId }) => {
-  const { listOfPopUps, popUpHandler, closePopUps } = useContext(popUpsContext);
+  const navigate = useNavigate();
 
   return (
     <div className='relative flex flex-col h-full w-3/12 bg-darkPurple'>
       {/* User container */}
       <div className='flex justify-between w-full h-32 pl-6 pt-4 pr-4 pb-4 bg-grayLight'>
-        <img
-          src={UserImage}
-          alt=''
-          className='w-20 h-20 circle cursor-pointer transition hover:backdrop-blur-lg hover:opacity-95'
-        />
-        <div className='flex relative'>
-          <PopUp
-            isShown={listOfPopUps[0].state}
-            Ref={listOfPopUps[0].popup}
-            right={16}
-            content={[
-              {
-                id: 0,
-                label: 'Start new chat',
-                action: () => console.log('new chat'),
-              },
-            ]}
+        {user.profileImage ? (
+          <img
+            //crossOrigin='anonymous'
+            src={`http://localhost:8000/${user.profileImage}`}
+            alt=''
+            className='w-20 h-20 object-cover circle cursor-pointer'
           />
-          <PopUp
-            isShown={listOfPopUps[1].state}
-            Ref={listOfPopUps[1].popup}
-            content={[
-              {
-                id: 0,
-                label: 'Log out',
-                action: () => console.log('log out'),
-              },
-            ]}
-          />
-
-          <FontAwesomeIcon
-            ref={listOfPopUps[0].icon}
-            onClick={() => popUpHandler(listOfPopUps[0].id)}
-            icon={faMessage}
-            className='w-8 h-8 cursor-pointer p-3 mr-4 transition rounded-full text-grayDark hover:bg-gray z-20'
-          />
-          <FontAwesomeIcon
-            ref={listOfPopUps[1].icon}
-            onClick={() => popUpHandler(listOfPopUps[1].id)}
-            icon={faEllipsisVertical}
-            className='w-10 h-10 cursor-pointer p-3 transition rounded-full text-grayDark hover:bg-gray active:bg-grayDark z-20'
-          />
+        ) : (
+          <div></div>
+        )}
+        <div className='flex space-x-5'>
+          <PopUpElement icon={faMessage}>
+            <span
+              className='transition hover:bg-gray p-3'
+              onClick={() => {
+                setIsContactPanelOpen(!isContactPanelOpen);
+                setIsChatsPanelOpen(false);
+              }}
+            >
+              Contacts
+            </span>
+            <span
+              className='transition hover:bg-gray p-3'
+              onClick={() => {
+                setIsChatsPanelOpen(!isChatsPanelOpen);
+                setIsContactPanelOpen(false);
+              }}
+            >
+              Chats
+            </span>
+          </PopUpElement>
+          <PopUpElement icon={faEllipsisVertical}>
+            <span
+              className='transition hover:bg-gray p-3'
+              onClick={() => {
+                dispatch(clearChatOnLogOut());
+                dispatch(postLogOut());
+                navigate('/login', { replace: true });
+              }}
+            >
+              Log out
+            </span>
+          </PopUpElement>
         </div>
       </div>
       {/* Search input */}
@@ -76,7 +83,6 @@ const LeftPanel: React.FC<LeftPopUpProps> = ({ UserImage, onSelectChatId }) => {
             type='text'
             name=''
             id=''
-            onSelect={closePopUps}
             placeholder='Search'
             className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full h-12 rounded-xl outline-none placeholder-grayDark pl-16 pt-4 pr-4 pb-4 text-2xl  transition focus:shadow-input'
           />
@@ -85,14 +91,8 @@ const LeftPanel: React.FC<LeftPopUpProps> = ({ UserImage, onSelectChatId }) => {
       {/* Chat cards container */}
       <div className='flex flex-col grow items-center w-full h-0 pt-5 bg-grayReg overflow-y-auto'>
         {/* Card */}
-        {[1, 2].map((e) => (
-          <ChatCard
-            key={e}
-            id={e}
-            onSelectChatId={onSelectChatId}
-            UserImage=''
-          />
-        ))}
+        {isChatsPanelOpen && <ChatsPanel />}
+        {isContactPanelOpen && <ContactsPanel />}
       </div>
     </div>
   );
