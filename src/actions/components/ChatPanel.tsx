2f5d072865e7c9ba0e712297e31d68b014ed58ab
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-02 17:00:27 -0700

    [add] image when null

--- src/actions/components/ChatPanel.tsx
+++ src/actions/components/ChatPanel.tsx
@@ -48,7 +48,25 @@ const ChatPanel: React.FC = () => {
               className='w-20 h-20 mr-6 circle cursor-pointer transition hover:backdrop-blur-lg hover:opacity-95'
             />
           ) : (
-            <div></div>
+            <div className='w-20 h-20 circle'>
+              <svg
+                className='w-20 h-20'
+                viewBox='0 0 18 20'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  fillRule='evenodd'
+                  clipRule='evenodd'
+                  d='M9 0C6.23858 0 4 2.23858 4 5C4 7.76142 6.23858 10 9 10C11.7614 10 14 7.76142 14 5C14 2.23858 11.7614 0 9 0ZM6 5C6 3.34315 7.34315 2 9 2C10.6569 2 12 3.34315 12 5C12 6.65685 10.6569 8 9 8C7.34315 8 6 6.65685 6 5Z'
+                  fill='#474747'
+                />
+                <path
+                  d='M5 12C3.67392 12 2.40215 12.5268 1.46447 13.4645C0.526784 14.4021 0 15.6739 0 17V19C0 19.5523 0.447715 20 1 20C1.55228 20 2 19.5523 2 19V17C2 16.2043 2.31607 15.4413 2.87868 14.8787C3.44129 14.3161 4.20435 14 5 14H13C13.7956 14 14.5587 14.3161 15.1213 14.8787C15.6839 15.4413 16 16.2044 16 17V19C16 19.5523 16.4477 20 17 20C17.5523 20 18 19.5523 18 19V17C18 15.6739 17.4732 14.4021 16.5355 13.4645C15.5979 12.5268 14.3261 12 13 12H5Z'
+                  fill='#474747'
+                />
+              </svg>
+            </div>
           )}
           <h3 className='text-3xl'>{alias}</h3>
         </div>
@@ -78,13 +96,33 @@ const ChatPanel: React.FC = () => {
       </div>
       {/* chat area */}
       <div className='w-full h-0 grow bg-white overflow-y-auto'>
-        {chat.map((e: any) => {
-          if (e.senderId === user.uid) {
-            return <MsgBubble key={e.mid} content={e.content} isRight={true} />;
-          } else {
-            return <MsgBubble key={e.mid} content={e.content} />;
+        {chat.map(
+          (e: {
+            mid: string;
+            content: string;
+            creationDate: string;
+            senderId: string;
+          }) => {
+            if (e.senderId === user.uid) {
+              return (
+                <MsgBubble
+                  key={e.mid}
+                  content={e.content}
+                  isRight={true}
+                  date={e.creationDate}
+                />
+              );
+            } else {
+              return (
+                <MsgBubble
+                  key={e.mid}
+                  content={e.content}
+                  date={e.creationDate}
+                />
+              );
+            }
           }
-        })}
+        )}
       </div>
       {/* Message input */}
       <div className='w-full h-24 flex justify-start items-center pl-5 pr-5 pt-3 pb-3 bg-grayDark'>
