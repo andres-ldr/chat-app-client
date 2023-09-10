2100943b5642bf481d1ba0f7be9753c0991ea900
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-09 18:26:53 -0700

    [edit] close event

--- src/actions/components/newChatElements/NewChatDialog.tsx
+++ src/actions/components/newChatElements/NewChatDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useEffect, useRef } from 'react';
 import Input from '../FormElements/input';
 import { useForm } from '../../hooks/form-hook';
 import Button from '../FormElements/Button';
@@ -13,10 +13,12 @@ import { selectNewChatDialog } from '../../redux/newChatDialog/newChatDialogSele
 import { toggleNewChatDialog } from '../../redux/newChatDialog/newChatDialogSlice';
 import { postNewContact } from '../../redux/contact/contactsSlice';
 import { AppDispatch } from '../../redux/store';
+import { selectContacts } from '../../redux/contact/contactsSelector';
 
 const NewChatDialog: React.FC = () => {
   const newChatDialogIsVisible = useSelector(selectNewChatDialog);
-  const newChatDialogState = useSelector(selectNewChatDialog);
+  const newDialogElementRef = useRef<HTMLDivElement>(null);
+  const { isLoading, error } = useSelector(selectContacts);
   const dispatch = useDispatch<AppDispatch>();
 
   const [formState, inputHandler] = useForm(
@@ -33,6 +35,22 @@ const NewChatDialog: React.FC = () => {
     false
   );
 
+  useEffect(() => {
+    const handler = (e: any) => {
+      if (newDialogElementRef.current) {
+        if (!newDialogElementRef.current.contains(e.target)) {
+          dispatch(toggleNewChatDialog(false));
+        }
+      }
+    };
+
+    document.addEventListener('mousedown', handler);
+
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  });
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -40,54 +58,63 @@ const NewChatDialog: React.FC = () => {
     const email = formState.inputs.email.value;
 
     dispatch(postNewContact({ alias, email }));
+
+    if (!isLoading) dispatch(toggleNewChatDialog(false));
   };
 
   return (
     <div
+      ref={newDialogElementRef}
       className={`${
         newChatDialogIsVisible ? 'flex' : 'hidden'
-      } absolute flex-col justify-center items-center w-[500px] h-1/2 p-10 bg-grayLight rounded-2xl space-y-10 animate-showDialog z-30`}
+      } absolute flex-col justify-center items-center w-[500px] h-1/2 p-10 bg-grayLight shadow-lg rounded-2xl space-y-10 animate-showDialog z-30`}
     >
-      <h2 className='text-4xl font-medium'>Add new contact</h2>
-      <form className='w-3/4 flex flex-col space-y-32'>
-        <div className='space-y-7'>
-          <Input
-            element='input'
-            id='alias'
-            type='text'
-            validators={[
-              VALIDATOR_REQUIRE(),
-              VALIDATOR_MINLENGTH(2),
-              VALIDATOR_MAXLENGTH(20),
-            ]}
-            placeholder='Alias'
-            onInput={inputHandler}
-          />
-          <Input
-            element='input'
-            id='email'
-            type='text'
-            validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
-            placeholder='Email'
-            onInput={inputHandler}
-          />
-        </div>
-        <div className='w-full h-auto flex flex-col justify-between space-y-6'>
-          <button
-            className='text-3xl h-16 leading-none font-bold p-3 transition rounded-xl hover:bg-grayReg'
-            type='submit'
-            onClick={(event) => {
-              event.preventDefault();
-              dispatch(toggleNewChatDialog(!newChatDialogState));
-            }}
-          >
-            Cancel
-          </button>
-          <Button onClick={handleSubmit} disabled={!formState.isValid}>
-            Create chat
-          </Button>
-        </div>
-      </form>
+      {!isLoading && (
+        <Fragment>
+          <h2 className='text-4xl font-medium'>Add new contact</h2>
+          <form className='w-3/4 flex flex-col space-y-32'>
+            <div className='space-y-7'>
+              <Input
+                element='input'
+                id='alias'
+                type='text'
+                validators={[
+                  VALIDATOR_REQUIRE(),
+                  VALIDATOR_MINLENGTH(2),
+                  VALIDATOR_MAXLENGTH(20),
+                ]}
+                placeholder='Alias'
+                onInput={inputHandler}
+              />
+              <Input
+                element='input'
+                id='email'
+                type='text'
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
+                placeholder='Email'
+                onInput={inputHandler}
+              />
+            </div>
+            <div className='w-full h-auto flex flex-col justify-between space-y-6'>
+              <button
+                className='text-3xl h-16 leading-none font-bold p-3 transition rounded-xl hover:bg-grayReg'
+                type='submit'
+                onClick={(event) => {
+                  event.preventDefault();
+                  dispatch(toggleNewChatDialog(!newChatDialogIsVisible));
+                }}
+              >
+                Cancel
+              </button>
+              <Button onClick={handleSubmit} disabled={!formState.isValid}>
+                Create contact
+              </Button>
+            </div>
+          </form>
+        </Fragment>
+      )}
+      {isLoading && <h1>Loading</h1>}
+      {/* {error && <h1>{error}</h1>} */}
     </div>
   );
 };
