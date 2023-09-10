5ecffa5625058785bae9eb33922d4f3184a9894a
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-09 19:14:45 -0700

    [fix] delete react

--- src/actions/components/LoginElements/CentralLoginSection.tsx
+++ src/actions/components/LoginElements/CentralLoginSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import LeftLoginSection from './LeftLoginSection';
 import LoginSection from './LoginSection';
 
--- src/actions/components/LoginElements/DivisionLine.tsx
+++ src/actions/components/LoginElements/DivisionLine.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const DivisionLine = () => {
   return (
     <div className='flex justify-center mb-10'>
