2e86bd82de137a7a2ce3efac9d42934fcf9e811b
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-09 18:19:38 -0700

    [delete] auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
-import { AuthProvider } from './actions/context/AuthContext.tsx';
+import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './actions/redux/store.ts';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 import './index.css';
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
       </PersistGate>
     </Provider>
   </BrowserRouter>
