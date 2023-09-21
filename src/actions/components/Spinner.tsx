7b250e3b60dcc92065b951d7cdff848bf8e2ea8a
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-21 11:43:21 -0700

    [add] spinner

---
+++ src/actions/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner: React.FC = () => {
+  return (
+    <div className='fixed w-screen h-screen bg-gray bg-opacity-25 flex justify-center items-center text-center'>
+      <div className='w-20 h-20 rounded-full border-8 border-purple-500 border-r-white border-l-white border-b-white animate-spin'></div>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/actions/views/Panel.tsx
+++ src/actions/views/Panel.tsx
@@ -1,15 +1,25 @@
 import NewChatDialog from '../components/newChatElements/NewChatDialog';
+import { selectContacts } from '../redux/contact/contactsSelector';
+import { selectChats } from '../redux/chats/chatsSelector';
 import { selectUser } from '../redux/user/userSelector';
 import RightPanel from '../components/RightPanel';
 import LeftPanel from '../components/LeftPanel';
+import Spinner from '../components/Spinner';
 import { useSelector } from 'react-redux';
 import React, { Fragment } from 'react';
+import { selectChat } from '../redux/chat/selectChat';
 
 const Panel: React.FC = () => {
   const { isLoading } = useSelector(selectUser);
+  const { isLoading: isLoadingChat } = useSelector(selectChat);
+  const { isLoading: isLoadingChats } = useSelector(selectChats);
+  const { isLoading: isLoadingContacts } = useSelector(selectContacts);
 
   return (
     <div className='relative flex flex-col items-center justify-center w-full h-screen p-10 bg-gradient-radial from-darkPurple to-brightPurple overflow-hidden'>
+      {isLoadingChats && <Spinner />}
+      {isLoadingContacts && <Spinner />}
+      {isLoadingChat && <Spinner />}
       {!isLoading && (
         <Fragment>
           <NewChatDialog />
@@ -19,6 +29,7 @@ const Panel: React.FC = () => {
           </div>
         </Fragment>
       )}
+      {isLoading && <Spinner />}
     </div>
   );
 };
