aabc650678e092c7c60a1b16dc5dbd205f206796
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-27 12:07:35 -0700

    [wip] auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { selectUser } from './actions/redux/user/userSelector';
 import NotFoundPage from './actions/views/NotFoundPage';
 import { Routes, Route } from 'react-router-dom';
 import Signup2 from './actions/views/Signup2';
 import SetImg from './actions/views/SetImg';
 import Signup from './actions/views/Signup';
+import { useSelector } from 'react-redux';
 import Login from './actions/views/Login';
 import Panel from './actions/views/Panel';
 import Home from './actions/views/Home';
 
 function App() {
+  const { user } = useSelector(selectUser);
+
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
@@ -15,7 +19,7 @@ function App() {
       <Route path='/signup' element={<Signup />}></Route>
       <Route path='/setimg' element={<SetImg />}></Route>
       <Route path='/signup-step-2' element={<Signup2 />}></Route>( (
-      <Route path='/panel' element={<Panel />}></Route>
+      {user && <Route path='/panel' element={<Panel />}></Route>}
       )
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
