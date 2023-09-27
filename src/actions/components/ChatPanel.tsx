edea93f8a095f75432cd8e8bbec48347e75cdba2
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-27 11:52:28 -0700

    [edit] input style and validation

--- src/actions/components/ChatPanel.tsx
+++ src/actions/components/ChatPanel.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm, useFormState } from 'react-hook-form';
 import { selectUser } from '../redux/user/userSelector';
 // import { VALIDATOR_REQUIRE } from '../Util/validators';
 import { useDispatch, useSelector } from 'react-redux';
@@ -32,7 +32,10 @@ type Inputs = {
 };
 
 const ChatPanel: React.FC = () => {
-  const { register, handleSubmit, setValue } = useForm<Inputs>();
+  const { register, handleSubmit, setValue, control } = useForm<Inputs>({
+    defaultValues: { message: '' },
+  });
+  const { dirtyFields } = useFormState({ control });
 
   const { chat, cid, chatImage, alias } = useSelector(selectChat);
   const { user } = useSelector(selectUser);
@@ -176,7 +179,7 @@ const ChatPanel: React.FC = () => {
         >
           <input
             className='w-full rounded-xl outline-none placeholder-grayDark pl-6 pt-4 pr-4 pb-4 text-2xl  transition resize-none focus:shadow-input'
-            {...register('message')}
+            {...register('message', { required: true })}
           />
           {/* <Input
             element='textarea'
@@ -194,7 +197,20 @@ const ChatPanel: React.FC = () => {
           >
             Send
           </Button> */}
-          <input className='button--rounded' type='submit' />
+          <label
+            className={`button--rounded ${
+              !dirtyFields.message ? 'button--disabled' : 'button--abled'
+            }`}
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='w-1/2 h-auto absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10 fill-grayReg'
+              viewBox='0 0 512 512'
+            >
+              <path d='M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z' />
+            </svg>
+            <input className='absolute inset-0' type='submit' value='' />
+          </label>
         </form>
       </div>
     </div>
