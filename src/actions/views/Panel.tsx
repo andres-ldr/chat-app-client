9f2c4336b2fdf910f18b12d73cd2d0d2d5150de0
Andrés <lopez.andresrigoberto@gmail.com>
2023-09-27 12:05:53 -0700

    [add] error view

--- src/actions/views/Panel.tsx
+++ src/actions/views/Panel.tsx
@@ -2,25 +2,36 @@ import NewChatDialog from '../components/newChatElements/NewChatDialog';
 import { selectContacts } from '../redux/contact/contactsSelector';
 import { selectChats } from '../redux/chats/chatsSelector';
 import { selectUser } from '../redux/user/userSelector';
+import { selectChat } from '../redux/chat/selectChat';
 import RightPanel from '../components/RightPanel';
 import LeftPanel from '../components/LeftPanel';
+import ErrorView from '../components/ErrorView';
 import Spinner from '../components/Spinner';
 import { useSelector } from 'react-redux';
 import React, { Fragment } from 'react';
-import { selectChat } from '../redux/chat/selectChat';
 
 const Panel: React.FC = () => {
-  const { isLoading } = useSelector(selectUser);
-  const { isLoading: isLoadingChat } = useSelector(selectChat);
-  const { isLoading: isLoadingChats } = useSelector(selectChats);
-  const { isLoading: isLoadingContacts } = useSelector(selectContacts);
+  const { isLoading: isLoadingUser, error: errorUser } =
+    useSelector(selectUser);
+  const { isLoading: isLoadingChat, error: errorChat } =
+    useSelector(selectChat);
+  const { isLoading: isLoadingChats, error: errorChats } =
+    useSelector(selectChats);
+  const { isLoading: isLoadingContacts, error: errorContacts } =
+    useSelector(selectContacts);
 
   return (
     <div className='relative flex flex-col items-center justify-center w-full h-screen p-10 bg-gradient-radial from-darkPurple to-brightPurple overflow-hidden'>
-      {isLoadingChats && <Spinner />}
-      {isLoadingContacts && <Spinner />}
-      {isLoadingChat && <Spinner />}
-      {!isLoading && (
+      {(isLoadingChats ||
+        isLoadingContacts ||
+        isLoadingChat ||
+        isLoadingUser) && <Spinner />}
+      {(errorUser || errorChat || errorChats || errorContacts) && (
+        <ErrorView
+          errorMsg={errorUser || errorChat || errorChats || errorContacts}
+        />
+      )}
+      {!isLoadingUser && !errorUser && (
         <Fragment>
           <NewChatDialog />
           <div className='flex w-11/12 h-400 bg-white'>
@@ -29,7 +40,6 @@ const Panel: React.FC = () => {
           </div>
         </Fragment>
       )}
-      {isLoading && <Spinner />}
     </div>
   );
 };
