28b1dd14c330d5b54abdbb44c5679ecb8a7cae04
Andrés <lopez.andresrigoberto@gmail.com>
2024-06-02 16:35:01 -0700

    redirect to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import { selectUser } from './actions/redux/user/userSelector';
 import NotFoundPage from './actions/views/NotFoundPage';
 import { Routes, Route } from 'react-router-dom';
-import Signup2 from './actions/views/Signup2';
-import SetImg from './actions/views/SetImg';
-import Signup from './actions/views/Signup';
-import { useSelector } from 'react-redux';
 import Login from './actions/views/Login';
 import Panel from './actions/views/Panel';
-import Home from './actions/views/Home';
+import MultiStepForm from './actions/components/FormElements/MultiStepForm';
 
 function App() {
-  const { user } = useSelector(selectUser);
+  // const { user, isLoading } = useSelector(selectUser);
 
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/login' element={<Login />}></Route>
-      <Route path='/signup' element={<Signup />}></Route>
-      <Route path='/setimg' element={<SetImg />}></Route>
-      <Route path='/signup-step-2' element={<Signup2 />}></Route>( (
-      {user && <Route path='/panel' element={<Panel />}></Route>}
-      )
+      {/* <Route path='/' element={<Home />}></Route> */}
+      <Route path='/' element={<Login />}></Route>
+      <Route path='/signup' element={<MultiStepForm />}></Route>
+      <Route path='/panel' element={<Panel />}></Route>
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
 }
 
 export default App;
+
+// Esta auth el usuario
+// sí acceder a la ruta panel
+// no ocultar la ruta
