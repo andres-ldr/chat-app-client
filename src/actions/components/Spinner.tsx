f9047acd7eaf5f4031ac1aa2f4b2b59c6dd186d9
Andrés <lopez.andresrigoberto@gmail.com>
2024-06-02 16:36:27 -0700

    edit color

--- src/actions/components/Spinner.tsx
+++ src/actions/components/Spinner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner: React.FC = () => {
   return (
     <div className='fixed w-screen h-screen bg-gray bg-opacity-25 flex justify-center items-center text-center z-50'>
-      <div className='w-20 h-20 rounded-full border-8 border-purple-500 border-r-white border-l-white border-b-white animate-spin'></div>
+      <div className='w-20 h-20 rounded-full border-8 border-pink-500 border-r-white border-l-white border-b-white animate-spin'></div>
     </div>
   );
 };
