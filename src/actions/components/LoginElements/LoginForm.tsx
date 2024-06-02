7ee395141d6cc269cbdff15ffeac090efb96b4e1
Andrés <lopez.andresrigoberto@gmail.com>
2024-06-02 16:39:13 -0700

    use tankstack query

--- src/actions/components/LoginElements/LoginForm.tsx
+++ src/actions/components/LoginElements/LoginForm.tsx
@@ -1,70 +1,97 @@
-import { VALIDATOR_MAXLENGTH, VALIDATOR_REQUIRE } from '../../Util/validators';
-import { postLogIn } from '../../redux/user/userSlice';
-import { useForm } from '../../hooks/form-hook';
-import { AppDispatch } from '../../redux/store';
+import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import Button from '../FormElements/Button';
-import { useDispatch } from 'react-redux';
-import Input from '../FormElements/input';
-import React from 'react';
+import { useState } from 'react';
+import { useHttpClient } from '../../hooks/http-hook';
+import Spinner from '../Spinner';
+import { useLogin } from '../../../services/mutations';
+import { useUserStore } from '../../../store/userStore';
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 const LoginForm = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
-
-  const [formState, inputHandler] = useForm(
-    {
-      email: {
-        value: '',
-        isValid: false,
-      },
-      password: {
-        value: '',
-        isValid: false,
-      },
+  const { register, handleSubmit, reset } = useForm<Inputs>({
+    defaultValues: {
+      password: '',
+      email: '',
     },
-    false
-  );
+  });
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const { isLoading } = useHttpClient();
+  const [inputType, setInputType] = useState('password');
 
-    const email = formState.inputs.email.value;
-    const password = formState.inputs.password.value;
+  const loginMutation = useLogin();
+  const { setUser } = useUserStore();
 
-    dispatch(postLogIn({ email, password }));
-
-    navigate('/panel', { replace: true });
+  const toggleInputType = () => {
+    setInputType(inputType === 'password' ? 'text' : 'password');
   };
 
-  return (
-    <form
-      className='flex flex-col  mb-16 space-y-5'
-      onSubmit={(event) => handleSubmit(event)}
-    >
-      <Input
-        element='input'
-        id='email'
-        type='text'
-        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MAXLENGTH(20)]}
-        placeholder='Email'
-        errorText='Required'
-        onInput={inputHandler}
-      />
+  const onSubmit = async (data: Inputs) => {
+    loginMutation.mutate(data);
+    reset();
+  };
 
-      <Input
-        element='input'
-        id='password'
-        type='password'
-        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MAXLENGTH(20)]}
-        placeholder='Password'
-        errorText='Required'
-        onInput={inputHandler}
-      />
-      <Button onClick={handleSubmit} disabled={!formState.isValid}>
-        Log In
-      </Button>
-    </form>
+  if (loginMutation.isSuccess) {
+    setUser(loginMutation.data.data);
+    navigate('/panel', { replace: true });
+  }
+
+  return (
+    <>
+      {loginMutation.error && (
+        <div className='fixed inset-0 w-screen h-screen bg-grayDark bg-opacity-20 flex items-center justify-center'>
+          <div className='w-1/3 z-50 py-20 px-10 bg-white font-bold shadow-2xl animate-show'>
+            <button
+              type='button'
+              onClick={loginMutation.reset}
+              className='text-red'
+            >
+              Clear
+            </button>
+            <br />
+            {loginMutation.error.message}
+          </div>
+        </div>
+      )}
+      {loginMutation.isPending && <Spinner />}
+      <form className='flex flex-col gap-2' onSubmit={handleSubmit(onSubmit)}>
+        <div className='flex flex-col gap-4'>
+          <label className='font-semibold' htmlFor='name'>
+            Email:
+          </label>
+          <input
+            type='email'
+            placeholder='example@email.com'
+            className='py-2 px-4'
+            {...register('email', { required: true })}
+          />
+        </div>
+        <div className='flex flex-col gap-4'>
+          <label className='font-semibold' htmlFor='password'>
+            Password:
+          </label>
+          <input
+            type={inputType}
+            className='py-2 px-4'
+            {...register('password', { required: true })}
+          />
+          <button type='button' onClick={toggleInputType}>
+            See password
+          </button>
+        </div>
+        <button
+          type='submit'
+          className='bg-indigo-800 text-white rounded-md py-2 px-4'
+          disabled={isLoading}
+        >
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 
