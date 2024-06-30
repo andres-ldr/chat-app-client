6da3ad9f003e60f3661b6aaeb34e68df6f21390f
Andres <lopez.andresrigoberto@gmail.com>
2024-06-29 22:09:02 -0700

    add handlers to control panels

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
-
 interface SidebarProps {
   isContactVisible: boolean;
   isChatPanelVisible: boolean;
-  setIsContactVisible: React.Dispatch<React.SetStateAction<boolean>>;
-  setIsChatPanelVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  handleContactPanel: () => void;
+  handleChatPanel: () => void;
 }
 
 const Sidebar = ({
   isContactVisible,
   isChatPanelVisible,
-  setIsChatPanelVisible,
-  setIsContactVisible,
+  handleContactPanel,
+  handleChatPanel,
 }: SidebarProps) => {
   return (
     <div className='bg-slate-900 p-3 flex items-start flex-col w-60 overflow-y-auto shadow-lg pt-2 pl-1 gap-5'>
-      <span className='font-semibold text-xl'>Dashboard</span>
       <div className='w-full flex flex-col gap-1'>
         <button
           className={`${
             isContactVisible && 'bg-slate-950'
           } hover:bg-slate-950 rounded w-full text-start py-1 px-2 transition-colors duration-300 ease-in-out`}
-          onClick={() => {
-            setIsContactVisible(true);
-            setIsChatPanelVisible(false);
-          }}
+          onClick={handleContactPanel}
         >
           Contacts
         </button>
@@ -32,10 +26,7 @@ const Sidebar = ({
           className={`${
             isChatPanelVisible && 'bg-slate-950'
           } hover:bg-slate-950 rounded w-full text-start py-1 px-2 transition-colors duration-300 ease-in-out`}
-          onClick={() => {
-            setIsContactVisible(false);
-            setIsChatPanelVisible(true);
-          }}
+          onClick={handleChatPanel}
         >
           Chats
         </button>
