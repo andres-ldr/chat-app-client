9bba0cd1ba021846233f7ca114bfbf96de69a306
Andres <lopez.andresrigoberto@gmail.com>
2024-07-04 21:23:25 -0700

    edit styles

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -9,7 +9,7 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
+  // DialogTrigger,
 } from './ui/dialog';
 import { useState } from 'react';
 
@@ -23,33 +23,32 @@ const ContactList = ({ handleChatPanel }: ContactListProps) => {
     useState(false);
 
   return (
-    <div className='flex flex-col gap-4 w-96 py-2'>
-      <div className='flex flex-col gap-2'>
-        <Button
-          className='bg-blue-600 hover:bg-blue-800'
-          onClick={() => setIsCreateContactDialogOpen(true)}
-        >
-          Create New Contact
-        </Button>
+    <div className='flex flex-col gap-2 w-96 py-2'>
+      <Button
+        className='bg-blue-600 hover:bg-blue-800'
+        onClick={() => setIsCreateContactDialogOpen(true)}
+      >
+        Create New Contact
+      </Button>
 
-        <Dialog
-          open={isCreateContactDialogOpen}
-          onOpenChange={setIsCreateContactDialogOpen}
-        >
-          <DialogContent className='sm:max-w-[425px] bg-slate-950'>
-            <DialogHeader className='text-slate-100'>
-              <DialogTitle>Create A New Contact</DialogTitle>
-            </DialogHeader>
-            <DialogDescription>Create a new contact</DialogDescription>
-            <ContactForm onClose={() => setIsCreateContactDialogOpen(false)} />
-          </DialogContent>
-        </Dialog>
+      <Dialog
+        open={isCreateContactDialogOpen}
+        onOpenChange={setIsCreateContactDialogOpen}
+      >
+        <DialogContent className='sm:max-w-[425px] bg-slate-950'>
+          <DialogHeader className='text-slate-100'>
+            <DialogTitle>Create A New Contact</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>Create a new contact</DialogDescription>
+          <ContactForm onClose={() => setIsCreateContactDialogOpen(false)} />
+        </DialogContent>
+      </Dialog>
 
-        {useContactQuery.data &&
-          useContactQuery.data.map((contact: Contact) => (
-            <ContactCard contact={contact} handleChatPanel={handleChatPanel} />
-          ))}
-      </div>
+      {useContactQuery.data &&
+        useContactQuery.data.map((contact: Contact) => (
+          <ContactCard contact={contact} handleChatPanel={handleChatPanel} />
+        ))}
+
       {useContactQuery.data?.length === 0 && <p>No contacts</p>}
     </div>
   );
