1c6e8f9572e33bd5aa2697180c6f600e2b5048d6
Andres <lopez.andresrigoberto@gmail.com>
2024-07-04 21:25:08 -0700

    edit components

--- src/components/MessageBuuble.tsx
+++ src/components/MessageBuuble.tsx
@@ -1,23 +1,52 @@
 import { Message } from '../types/message';
 import moment from 'moment';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './ui/dialog';
+import { Button } from './ui/button';
+import { useState } from 'react';
+import { Socket } from 'socket.io-client';
+import { User } from '../types/user';
+import { useMessageSelectedStore } from '../store/messageStore';
 
 interface MessageBuubleProps {
+  socket: Socket;
   index: number;
-  user: any;
+  user: User | null;
   message: Message;
   messages: Message[];
-  handleEditMessage: any;
-  handleDeleteMessage: any;
 }
 
 const MessageBuuble = ({
+  socket,
   index,
   user,
   message: { content, senderId, sender, creationDate },
-  handleDeleteMessage,
-  handleEditMessage,
   messages,
 }: MessageBuubleProps) => {
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const { setMessage } = useMessageSelectedStore();
+
+  const handleEditMessage = (message: Message) => {
+    setMessage(message);
+  };
+
+  const handleDeleteMessage = (message: Message) => {
+    socket.emit('delete-message', message);
+  };
+
   const getDate = (date: Date) => {
     const time = moment(date).utc().format('HH:mm');
     return time;
@@ -26,40 +55,77 @@ const MessageBuuble = ({
   return (
     <div
       key={index}
-      className={`flex bg-slate-950 items-center gap-4 rounded-2xl py-2 px-4 ${
+      className={`flex bg-slate-950 items-center gap-4 rounded-xl py-1 px-4 ${
         user && senderId === user.uid ? 'self-end' : 'self-start'
       } `}
     >
       {sender && sender.uid !== user?.uid ? (
-        <img
-          src={`${import.meta.env.VITE_BACKEND_URL}${sender.profileImage}`}
-          alt={sender.name}
-          className='w-10 h-10 rounded-full object-cover'
-        />
+        <Avatar>
+          <AvatarImage
+            src={`${import.meta.env.VITE_BACKEND_URL}${sender.profileImage}`}
+            alt={sender.name}
+          />
+          <AvatarFallback>{sender.name[0]}</AvatarFallback>
+        </Avatar>
       ) : null}
-      <div className='flex flex-col'>
-        <p>{content}</p>
-        <div className='flex gap-4 justify-end items-center'>
-          {sender && sender.uid === user?.uid && (
-            <div className='flex gap-2'>
-              <button
-                className='text-yellow-500 font-bold'
-                onClick={() => handleEditMessage(messages[index])}
-              >
-                Edit
-              </button>
-              <button
-                className='text-red-500 font-bold'
-                onClick={() => handleDeleteMessage(messages[index])}
-              >
-                Delete
-              </button>
-            </div>
-          )}
 
+      <div className='flex gap-2'>
+        <p>{content}</p>
+        <div className='flex flex-col gap-2 justify-center'>
+          {sender && sender.uid === user?.uid && (
+            <DropdownMenu>
+              <DropdownMenuTrigger>...</DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem
+                  className='text-yellow-500 font-semibold'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleEditMessage(messages[index]);
+                  }}
+                >
+                  Edit
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className='text-red-500 font-semibold'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setIsDeleteDialogOpen(true);
+                  }}
+                >
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
           <p className='text-xs text-slate-400'>{getDate(creationDate)}</p>
         </div>
       </div>
+
+      <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
+        {isDeleteDialogOpen && (
+          <DialogContent onClick={(e) => e.stopPropagation()}>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. Are you sure you want to
+                permanently delete this file from our servers?
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button
+                className='bg-red-500 hover:bg-red-700'
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDeleteMessage(messages[index]);
+                  setIsDeleteDialogOpen(false);
+                }}
+              >
+                Confirm
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        )}
+      </Dialog>
     </div>
   );
 };
